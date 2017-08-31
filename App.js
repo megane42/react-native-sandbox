0db8d7043ba29ae6c732b090c005614f139e5eda
megane42 <h.kzm.gm@gmail.com>
2017-09-01 00:23:35 +0900

    asdf

--- App.js
+++ App.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react';
-import { Alert, StyleSheet, Image, TouchableHighlight, View } from 'react-native';
+import { Alert, StyleSheet, Image, TouchableHighlight, View, ScrollView } from 'react-native';
 
-export default class Touchables extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {isIdolized: false};
-  }
 
-  _onPressButton = () => {
-      this.setState({isIdolized: !this.state.isIdolized});
-  }
-
-  _getUri = () => {
-      return this.state.isIdolized ? "https://imcgdb.info/card-img/3432901.jpg" : "https://imcgdb.info/card-img/3532902.jpg"
-  }
-
-  render() {
-    let src = {uri: this._getUri()};
-    return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="blue">
-            <Image source={src} style={styles.idol}/>
-        </TouchableHighlight>
-      </View>
-    );
-  }
+class Idol extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isIdolized: false};
+    }
+    _onPressButton = () => {
+        this.setState({isIdolized: !this.state.isIdolized});
+    }
+    _getUri = () => {
+        return this.state.isIdolized ? this.props.idolizedUri : this.props.unidolizedUri;
+    }
+    render() {
+        let src = {uri: this._getUri()};
+        return (
+                <TouchableHighlight onPress={this._onPressButton} underlayColor="blue">
+                <Image source={src} style={styles.idol}/>
+                </TouchableHighlight>
+        );
+    }
 }
 
+
+export default class Main extends Component {
+    render() {
+        return (
+                <ScrollView maximumZoomScale={10}>
+                <Idol idolizedUri="https://imcgdb.info/card-img/3432901.jpg" unidolizedUri="https://imcgdb.info/card-img/3532902.jpg" />
+                <Idol idolizedUri="https://imcgdb.info/card-img/3432901.jpg" unidolizedUri="https://imcgdb.info/card-img/3532902.jpg" />
+                <Idol idolizedUri="https://imcgdb.info/card-img/3432901.jpg" unidolizedUri="https://imcgdb.info/card-img/3532902.jpg" />
+                </ScrollView>
+        );
+    }
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    backgroundColor: '#555555'
-  },
-  idol: {
-      width: 320,
-      height: 400,
-  },
+    idol: {
+        width: 320,
+        height: 400,
+    },
 })
