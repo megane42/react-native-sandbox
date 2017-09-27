3a9eaf4a8efceb985ac81681ede5cb8241b17198
megane42 <h.kzm.gm@gmail.com>
2017-09-28 00:53:11 +0900

    install react-native-map

--- App.js
+++ App.js
@@ -1,45 +1,41 @@
 import React, { Component } from 'react';
-import { Alert, StyleSheet, Image, TouchableHighlight, View, ScrollView } from 'react-native';
-
-
-class Idol extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isIdolized: false};
-    }
-    _onPressButton = () => {
-        this.setState({isIdolized: !this.state.isIdolized});
-    }
-    _getUri = () => {
-        return this.state.isIdolized ? this.props.idolizedUri : this.props.unidolizedUri;
-    }
-    render() {
-        let src = {uri: this._getUri()};
-        return (
-                <TouchableHighlight onPress={this._onPressButton} underlayColor="blue">
-                <Image source={src} style={styles.idol}/>
-                </TouchableHighlight>
-        );
-    }
-}
-
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class Main extends Component {
-    render() {
-        return (
-                <ScrollView maximumZoomScale={10}>
-                <Idol idolizedUri="https://imcgdb.info/card-img/3432901.jpg" unidolizedUri="https://imcgdb.info/card-img/3532902.jpg" />
-                <Idol idolizedUri="https://imcgdb.info/card-img/3432901.jpg" unidolizedUri="https://imcgdb.info/card-img/3532902.jpg" />
-                <Idol idolizedUri="https://imcgdb.info/card-img/3432901.jpg" unidolizedUri="https://imcgdb.info/card-img/3532902.jpg" />
-                </ScrollView>
-        );
-    }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        lat: 0,
+        lng: 0,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => {
+        this.setState({
+            lat: location.coords.latitude,
+            lng: location.coords.longitude,
+        });
+    });
+  }
+
+  render() {
+    return (
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: this.state.lat,
+          longitude: this.state.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      />
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    idol: {
-        width: 320,
-        height: 400,
-    },
-})
+  map: { ...StyleSheet.absoluteFillObject, },
+});
